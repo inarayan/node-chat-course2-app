e96011b6e9e4075000e938051c6aca2d4a4bca2b
Indra Bijay <indrabijay007@gmail.com>
2018-05-23 23:35:15 -0400

    Fixed the hardcoded port issue

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log("Server started on port: " + `${port}`);
 })
