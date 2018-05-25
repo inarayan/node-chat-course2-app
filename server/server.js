0501fbc33fca4912a2dad0413a96f989ef284e94
Indra Bijay <indrabijay007@gmail.com>
2018-05-25 17:42:50 -0400

    Added the socket code

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 var path = require('path');
 var express = require('express');
+var io = require('socket.io');
+var http = require('http');
 
 var app = express();
 var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var socketIO = io(server);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(port, ()=>{
+
+socketIO.on('connection', function(clientSocket){
+    console.log("Connected to the client..." + clientSocket);
+    clientSocket.on('disconnect', function(){
+    console.log("disconnected from the client");
+})
+});
+
+
+
+server.listen(port, ()=>{
     console.log("Server started on port: " + `${port}`);
 })
